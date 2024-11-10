164e846b9fc610f93fe098e9be282769f7a0b8db
ucfusl6 <ucfusl6@ucl.ac.uk>
2024-11-10 22:19:25 +0000

    wk5 Homework

--- wk_4homework.R
+++ wk_4homework.R
@@ -1,116 +1,149 @@
-#Week 4 Homework
-################
+# Ineqaulity task - week 4
 
-library(usethis)          #not needed but useful to create Github repository
-library(here)             #for specifying paths
-library(janitor)          #clean names functionality
-library(countrycode)      #standardises country names and codes
-library(sf)               #spatial features
-library(dplyr)            #mutate, filter etc
-library(tmap)             #thematic maps
+### Read in data:
 
-######################## 
+#Located within the Data folder:
+  
+#* HDI data from: https://hdr.undp.org/data-center/documentation-and-downloads
+#* Shapefile from: https://hub.arcgis.com/datasets/2b93b06dc0dc4e809d3c8db5cb96ba69_0/explore?location=-2.688200%2C0.000000%2C1.41 
 
-#PART 1 - LOAD DATA AND BOUNDARIES
+## Load packages
 
-#Load in spatial data (json format)
+library(tidyverse)
+library(sf)
+library(here)
+library(janitor)
+library(countrycode)
 
-RawWorldMap <- st_read(here("World_Countries_(Generalized)_9029012925078512962.geojson"))
-
-WorldMap <- RawWorldMap %>%
-  clean_names()
+## read in data
 
 
-plot(WorldMap$geometry)
-head(WorldMap)
+HDI <- read_csv(here::here("/Users/shinliu/Desktop/CASA0005/Week 4/prac 1/wk4_paer1","HDR23-24_Composite_indices_complete_time_series.csv"),
+                locale = locale(encoding = "latin1"),
+                na = " ", skip=0)
+
+World <- st_read(here("/Users/shinliu/Desktop/CASA0005/Week 4/prac 1/wk4_paer1", "World_Countries_(Generalized)_9029012925078512962.geojson"))
+
+
+## Column names
+
+#* World shape has the column ISO2
+#* HDI has ISO3
+#* We could join on country name?
+  
+#  Select columns we want and change country name to country code, using the `countrycode` package
+
+#We could also just join it without this on the country name column 
+
+#Some notes:
+  
+#* COW codes = correlates of war codes
+#* ISO = International Organization for Standardization with 2 or 3 letters...
+
+
+HDIcols<- HDI %>%
+  clean_names()%>%
+  select(iso3, country, gii_2019, gii_2010)%>%
+  mutate(difference=gii_2019-gii_2010)%>%
+  #not needed here as we can now use the country name...but see below
+  mutate(iso_code=countrycode(country, origin = 'country.name', destination = 'iso2c'))%>%
+  mutate(iso_code2=countrycode(iso3, origin ='iso3c', destination = 'iso2c'))
+
+
+## Join
+
+#Join the csv to world shape file
+
+
+Join_HDI <- World %>% 
+  clean_names() %>%
+  left_join(., 
+            HDIcols,
+            # change to "aff_iso" = "iso_code"
+            by = c("iso" = "iso_code"))
+
+# 261 if using "aff_iso", 251 if using "iso". Could filter out the NA values.
+
+
+#If using affiliate code then Aruba (country) is part of the Netherlands (affiliate country).
+
+#Aruba is a self-governing country within the Kingdom of the Netherlands.
+
+#If using iso code then Aruba will have no data as it has no data in the HDI. 
+
+#We could also use the country column to join...
+
+
+Join_HDI_2 <- World %>% 
+  clean_names() %>%
+  left_join(., 
+            HDIcols,
+            by = c("country" = "country"))
+#251
+
+```
+
+## Problems 
+
+#* With Join_HDI there are 11 rows for Antarctica **if using aff_iso** - be sure to use iso column or remove the NA values as in the aff_iso Antarctica is NA. Within the HDI dataset there are a series of rows for collections (e.g. World and Very high human development counties). So this join is being done on the `NA` values. For example, the `aff_iso` for Antarctica is NA and the ISO code for Very high human development counties is also NA!
+  
+  
+#* With Join_HDI_2 (that uses country name to join) the entries for other islands aren't joined as they don't have the same name...for example, Anguilla, Bermuda, Falkland Islands..... 
+
+#These are The British Overseas Territories (BOTs), also known as the United Kingdom Overseas Territories (UKOTs), fourteen territories with a constitutional and historical link with the United Kingdom, but do not form part of the United Kingdom itself.
+
+#Should the HDI value for the UK be assigned to the The British Overseas Territories ?
+  
+
+Join_HDI_GB<-Join_HDI %>%
+  filter(aff_iso=="GB")
+
+Join_HDI_2_GB<-Join_HDI_2 %>%
+  filter(aff_iso=="GB")
+
+
+#..next time we will make a map using different repos
+
+
+## Making Maps
+library(tmap)
+library(tmaptools)
+library(tidyverse)
+library(dplyr)
+#library(leafpop)
+#library(leaflet)
+
+
+tmap_mode("plot")
+
+# set the breaks
+# for our mapped data
 
 
 
-#WorldMapSimp <- st_simplify(WorldMap, dTolerance = 1000)
-#plot(WorldMapSimp$geometry)
+# plot map
+#Gender_Inq_map <- tm_shape(Join_HDI_2) + 
+#  tm_polygons("difference", 
+#              breaks=breaks,
+#              palette="PuBu")+
+#  tm_legend(show=FALSE)+
+#  tm_layout(frame=FALSE)+
+#  tm_credits("(a)", position=c(0,0.85), size=1.5)
+breaks = c(-Inf,-0.5,-0.3,-0.1,0.1,0.3) 
 
+Gender_Inq_map <- tm_shape(Join_HDI_2) + 
+tm_polygons("difference",
+            breaks = c(-Inf,-0.5,-0.3,-0.1,0.1,0.3),
+            style="pretty",
+            palette="Blues",
+            midpoint=0,
+            title="difference 2010-2019",
+            labels = c("0.1 to 0.3", "-0.1 to 0.1", "-0.3 to -0.1", "-0.5 to -0.3", "< -0.5"),  # reverse the labels,
+            fill_alpha = 0.9) +
+  tm_scale_bar(position=c("left", "bottom"), text.size=0.6)+
+  tm_layout(title = "difference in gender inequality", legend.position = c("right", "bottom"))
 
-#Load in gender inequality data
-
-RawGenderData <- read.csv(here("gender_inequality_index.csv"),
-                          header = TRUE,
-                          sep = ",")
-
-#alt method for reading csv in
-RawTest <- read.csv("gender_inequality_index.csv")
-
-GenderData <- RawGenderData %>%
-  clean_names()
-
-########################
-
-#PART 2 - GENDER INEQUALITY DIFFERENCE CALULATED
-
-GenderSubset <- subset(GenderData, select = c(iso3, country, hdicode, region,
-                                              gii_2010, gii_2019))
-
-GenderSubsetDiff <- GenderSubset %>%
-  mutate(gii_difference = (gii_2019 - gii_2010))
-
-head(GenderSubsetDiff)
-
-########################
-
-#PART 3 - SORT OUT COUNTRY CODES
-
-GenderSubsetDiffCCode <- GenderSubsetDiff %>%
-  mutate(ISO2code = countrycode(iso3, "iso3c", "iso2c"))
-
-GenderSubsetDiffCCode <- GenderSubsetDiffCCode %>%
-  rename("iso" = "ISO2code")    #rename with dplyr - new column name = old column name
-
-########################
-
-#PART 4- JOIN DATA AND BOUNDARIES
-
-GIIMap <- WorldMap %>%
-  merge(.,
-        GenderSubsetDiffCCode,
-        by.x ="iso",
-        by.y ="iso",
-        all.x = TRUE,
-        no.dups = TRUE)
-
-########################
-
-#PART 5 MAP
-
-qtm(GIIMap, 
-    fill = "gii_difference")
-
-# interactive map of Gender Inequality Index (Differences between 2010 and 2019)
-tmap_mode("view")
-tm_shape(GIIMap) + 
-  tm_polygons("gii_difference", 
-              id = "country.x",
-              #style="sd",
-              breaks = c(-Inf,-0.5,-0.3,-0.1,0.1,0.3),
-              palette="RdYlGn",
-              midpoint=0,
-              title="Diff. between GI Index 2010-2019",
-              labels = c("0.1 to 0.3", "-0.1 to 0.1", "-0.3 to -0.1", "-0.5 to -0.3", "< -0.5"),  # reverse the labels
-              fill_alpha = 0.9) + 
-  #tm_compass(position = c("left", "bottom"),type = "arrow") + 
-  tm_scale_bar(position = c("left", "bottom")) +
-  tm_layout(title = "Difference in GI Index 2010-2019")
-
-########################
-
-#Sources of Data
-
-#https://hdr.undp.org/data-center/documentation-and-downloads accessed 27/10/2024
-#https://hub.arcgis.com/datasets/2b93b06dc0dc4e809d3c8db5cb96ba69_0/explore?location=-2.688200%2C0.000000%2C1.41 accessed 27/10/24
-
-
-#websites used
-
-#https://sparkbyexamples.com/r-programming/dplyr-rename-column/
-#https://rpubs.com/jenrichmond/clean_names
-#https://stackoverflow.com/questions/54297542/r-tmap-changing-labels-in-view-mode
+t
+print(Gender_Inq_map)
 
 
